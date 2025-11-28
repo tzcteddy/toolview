7dd8f7e63a9305ce090ec26110836360e4bc722b
tzcteddy <1342671790@qq.com>
2025-11-28 17:40:55 +0800

    feat: up

--- dist/online/server/entry.mjs
+++ dist/online/server/entry.mjs
@@ -1,18 +1,26 @@
 import { renderers } from './renderers.mjs';
-import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CuKVFEwl.mjs';
-import { manifest } from './manifest_DpPJdxwv.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_dWrkg7la.mjs';
+import { manifest } from './manifest_gQYRzaoh.mjs';
 
 const serverIslandMap = new Map();;
 
-const _page0 = () => import('./pages/tools.astro.mjs');
-const _page1 = () => import('./pages/wechat/detail/_title_.astro.mjs');
-const _page2 = () => import('./pages/wechat.astro.mjs');
-const _page3 = () => import('./pages/index.astro.mjs');
+const _page0 = () => import('./pages/bg.astro.mjs');
+const _page1 = () => import('./pages/dev.astro.mjs');
+const _page2 = () => import('./pages/index1.astro.mjs');
+const _page3 = () => import('./pages/open-source.astro.mjs');
+const _page4 = () => import('./pages/tools.astro.mjs');
+const _page5 = () => import('./pages/wechat/detail/_title_.astro.mjs');
+const _page6 = () => import('./pages/wechat.astro.mjs');
+const _page7 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
-    ["src/pages/tools/index.astro", _page0],
-    ["src/pages/wechat/detail/[title].astro", _page1],
-    ["src/pages/wechat/index.astro", _page2],
-    ["src/pages/index.astro", _page3]
+    ["src/pages/bg.astro", _page0],
+    ["src/pages/dev/index.astro", _page1],
+    ["src/pages/index1.astro", _page2],
+    ["src/pages/open-source/index.astro", _page3],
+    ["src/pages/tools/index.astro", _page4],
+    ["src/pages/wechat/detail/[title].astro", _page5],
+    ["src/pages/wechat/index.astro", _page6],
+    ["src/pages/index.astro", _page7]
 ]);
 
 const _manifest = Object.assign(manifest, {

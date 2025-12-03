cec5ac7652b6ab6e379a214a578218f70230c695
tzcteddy <1342671790@qq.com>
2025-12-03 17:46:06 +0800

    feat: up

---
+++ dist/online/client/astro/index.BxTecECe.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["astro/index.CFNlq4po.js","astro/_plugin-vue_export-helper.DlAUqK2U.js","astro/runtime-core.esm-bundler.Ck478IpS.js","astro/index.BTfnfc8q.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./preload-helper.BS46-MCq.js";import{e as o}from"./runtime-core.esm-bundler.Ck478IpS.js";const n=o({loader:()=>e(()=>import("./index.CFNlq4po.js"),__vite__mapDeps([0,1,2,3])),loadingComponent:{template:"加载中..."},delay:500});export{n as default};
--- dist/online/server/entry.mjs
+++ dist/online/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
-import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_5LwwAan2.mjs';
-import { manifest } from './manifest_BXGWE3k5.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CJhReII6.mjs';
+import { manifest } from './manifest_CyHLCKbr.mjs';
 
 const serverIslandMap = new Map();;
 

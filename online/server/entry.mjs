167916e9a49557db2faa5b20ff6fddb3cb20e023
tzcteddy <1342671790@qq.com>
2026-09-24 15:55:56 +0800

    update

--- online/server/entry.mjs
+++ online/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_M_6GtUtO.mjs';
-import { manifest } from './manifest_L4zU8_Nx.mjs';
+import { manifest } from './manifest_CtnjqBto.mjs';
 
 const serverIslandMap = new Map();;
 
